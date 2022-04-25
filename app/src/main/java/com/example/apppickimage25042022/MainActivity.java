d5fdd2ef72388d4ce1e8590b1a1faa1bf5f0d29b
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-04-25 16:00:29 +0700

    create Handler and countdownTimer

---
+++ app/src/main/java/com/example/apppickimage25042022/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.apppickimage25042022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    Handler mHandler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        CountDownTimer countDownTimer=new CountDownTimer(10000,1000) {
+//            @Override
+//            public void onTick(long l) {
+//                if(l>1000)
+//                {
+//                    Log.d("BBB", l + "");
+//                }
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//
+//            }
+//
+//
+//        };
+//        countDownTimer.start();
+        mHandler=new Handler();
+    }
+
+    public void setDelay(long tinme)
+    {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        },1000);
+    }
+}
