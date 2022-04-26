e752db9e936e54522861291133fd92c4a50b39d8
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-04-26 12:58:10 +0700

    update source code

--- app/src/main/java/com/example/apppickimage25042022/MainActivity.java
+++ app/src/main/java/com/example/apppickimage25042022/MainActivity.java
@@ -21,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mProgressBar=findViewById(R.id.progressBarTime);
+        mTvPoint=findViewById(R.id.textViewPoint);
+        mImgRandom=findViewById(R.id.imgRandom);
+        mImgPick=findViewById(R.id.imgPick);
 
 
 
